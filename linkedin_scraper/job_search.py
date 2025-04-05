b01d36f7bd43ca505815c2b8da7b75bc3c8d6f79
Nicolas <nicolas.buton44@gmail.com>
2025-04-05 22:25:50 +0200

    Updated version for april 2025

--- linkedin_scraper/job_search.py
+++ linkedin_scraper/job_search.py
@@ -2,6 +2,7 @@ import os
 from typing import List
 from time import sleep
 import urllib.parse
+import time
 
 from .objects import Scraper
 from . import constants as c
@@ -9,6 +10,7 @@ from .jobs import Job
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
@@ -16,7 +18,14 @@ from selenium.webdriver.common.keys import Keys
 class JobSearch(Scraper):
     AREAS = ["recommended_jobs", None, "still_hiring", "more_jobs"]
 
-    def __init__(self, driver, base_url="https://www.linkedin.com/jobs/", close_on_complete=False, scrape=True, scrape_recommended_jobs=True):
+    def __init__(
+        self,
+        driver,
+        base_url="https://www.linkedin.com/jobs/",
+        close_on_complete=False,
+        scrape=True,
+        scrape_recommended_jobs=True,
+    ):
         super().__init__()
         self.driver = driver
         self.base_url = base_url
@@ -24,23 +33,43 @@ class JobSearch(Scraper):
         if scrape:
             self.scrape(close_on_complete, scrape_recommended_jobs)
 
-
     def scrape(self, close_on_complete=True, scrape_recommended_jobs=True):
         if self.is_signed_in():
-            self.scrape_logged_in(close_on_complete=close_on_complete, scrape_recommended_jobs=scrape_recommended_jobs)
+            self.scrape_logged_in(
+                close_on_complete=close_on_complete,
+                scrape_recommended_jobs=scrape_recommended_jobs,
+            )
         else:
             raise NotImplemented("This part is not implemented yet")
 
-
     def scrape_job_card(self, base_element) -> Job:
-        job_div = self.wait_for_element_to_load(name="job-card-list__title", base=base_element)
+        job_div = self.wait_for_element_to_load(
+            name="artdeco-entity-lockup__title", base=base_element
+        )
+        base_element.click()
         job_title = job_div.text.strip()
         linkedin_url = job_div.get_attribute("href")
-        company = base_element.find_element_by_class_name("artdeco-entity-lockup__subtitle").text
-        location = base_element.find_element_by_class_name("job-card-container__metadata-wrapper").text
-        job = Job(linkedin_url=linkedin_url, job_title=job_title, company=company, location=location, scrape=False, driver=self.driver)
-        return job
 
+        company = base_element.find_element(
+            By.CLASS_NAME, "artdeco-entity-lockup__subtitle"
+        ).text
+        location = base_element.find_element(
+            By.CLASS_NAME, "job-card-container__metadata-wrapper"
+        ).text
+        job_descriptions = self.driver.find_element(By.ID, "job-details").text
+        # job_descriptions = self.wait_for_element_to_load(
+        #     by=By.ID, name="job-details", base=base_element
+        # ).text
+        job = Job(
+            linkedin_url=linkedin_url,
+            job_title=job_title,
+            company=company,
+            location=location,
+            job_description=job_descriptions,
+            scrape=False,
+            driver=self.driver,
+        )
+        return job
 
     def scrape_logged_in(self, close_on_complete=True, scrape_recommended_jobs=True):
         driver = self.driver
@@ -48,30 +77,26 @@ class JobSearch(Scraper):
         if scrape_recommended_jobs:
             self.focus()
             sleep(self.WAIT_FOR_ELEMENT_TIMEOUT)
-            job_area = self.wait_for_element_to_load(name="scaffold-finite-scroll__content")
-            areas = self.wait_for_all_elements_to_load(name="artdeco-card", base=job_area)
+            job_area = self.wait_for_element_to_load(
+                name="scaffold-finite-scroll__content"
+            )
+            areas = self.wait_for_all_elements_to_load(
+                name="artdeco-card", base=job_area
+            )
             for i, area in enumerate(areas):
                 area_name = self.AREAS[i]
                 if not area_name:
                     continue
                 area_results = []
-                for job_posting in area.find_elements_by_class_name("jobs-job-board-list__item"):
+                for job_posting in area.find_elements_by_class_name(
+                    "jobs-job-board-list__item"
+                ):
                     job = self.scrape_job_card(job_posting)
                     area_results.append(job)
                 setattr(self, area_name, area_results)
         return
 
-
-    def search(self, search_term: str) -> List[Job]:
-        url = os.path.join(self.base_url, "search") + f"?keywords={urllib.parse.quote(search_term)}&refresh=true"
-        self.driver.get(url)
-        self.scroll_to_bottom()
-        self.focus()
-        sleep(self.WAIT_FOR_ELEMENT_TIMEOUT)
-
-        job_listing_class_name = "jobs-search-results-list"
-        job_listing = self.wait_for_element_to_load(name=job_listing_class_name)
-
+    def scroll_to_bottom_job_list(self, job_listing_class_name):
         self.scroll_class_name_element_to_page_percent(job_listing_class_name, 0.3)
         self.focus()
         sleep(self.WAIT_FOR_ELEMENT_TIMEOUT)
@@ -84,8 +109,52 @@ class JobSearch(Scraper):
         self.focus()
         sleep(self.WAIT_FOR_ELEMENT_TIMEOUT)
 
+    def search(self, search_term: str, geoid: int) -> List[Job]:
+        url = (
+            os.path.join(self.base_url, "search")
+            + f"?keywords={urllib.parse.quote(search_term)}&geoId={geoid}&refresh=true"
+        )
+        self.driver.get(url)
+        self.scroll_to_bottom()
+        self.focus()
+        sleep(self.WAIT_FOR_ELEMENT_TIMEOUT)
+
+        job_listing_class_name = "scaffold-layout__list"
+        job_listing = self.wait_for_element_to_load(name=job_listing_class_name)
+        job_listing = job_listing.find_element(By.XPATH, "./div[1]")
+        job_listing_class_name = str(job_listing.get_attribute("class")).replace(
+            "\n", ""
+        )
+        print(f"Class name of the first div: {job_listing_class_name}")
+        self.scroll_to_bottom_job_list(job_listing_class_name)
+
         job_results = []
-        for job_card in self.wait_for_all_elements_to_load(name="job-card-list", base=job_listing):
-            job = self.scrape_job_card(job_card)
-            job_results.append(job)
+        while True:
+            job_cards = self.wait_for_all_elements_to_load(
+                name="job-card-list", base=job_listing
+            )
+            for job_card in job_cards:
+                try:
+                    job = self.scrape_job_card(job_card)
+                    job_results.append(job)
+                except:
+                    print("Cannot parse this job")
+                time.sleep(5)
+            try:
+                next_page_button = self.driver.find_element(
+                    By.XPATH,
+                    # "//span[contains(@class, 'artdeco-button__text') and (contains(., 'Next') or contains(., 'Suivant'))]",
+                    # "//div[@class='pagination p4']/span[contains(@class, 'artdeco-button__text') and (contains(., 'Next') or contains(., 'Suivant'))]",
+                    "//div[contains(@class, 'jobs-search-pagination') and contains(@class, 'p4')]/button[contains(@class, 'artdeco-button') and .//*[contains(@class, 'artdeco-button__text') and (text()='Next' or text()='Suivant')]]",
+                )
+                next_page_button.click()
+                # Wait for at least one job card to load on the new page
+                self.wait_for_element_to_load(
+                    by=By.CLASS_NAME, name="job-card-list", base=job_listing
+                )
+                self.scroll_to_bottom_job_list(job_listing_class_name)
+            except NoSuchElementException:
+                # If the "Next" button is not found, we are on the last page
+                break
+
         return job_results
---
+++ linkedin_scraper/utils.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def list_of_job_to_pandas(job_list):
+    dico_df = {
+        "linkedin_url": [],
+        "job_title": [],
+        "company": [],
+        "company_linkedin_url": [],
+        "location": [],
+        "posted_date": [],
+        "applicant_count": [],
+        "job_description": [],
+        "benefits": [],
+    }
+    for job in job_list:
+        dico_this_job = job.to_dict()
+        for key in dico_df.keys():
+            dico_df[key].append(dico_this_job[key])
+    df = pd.DataFrame.from_dict(dico_df)
+    return df

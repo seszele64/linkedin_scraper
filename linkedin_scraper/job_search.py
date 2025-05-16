9032a6416c5547cc8ed5a19f76851b1796455794
seba <github@szelewi.cz>
2025-05-16 22:00:41 +0200

    refactor(job_search): add TimeoutException to exception imports

--- linkedin_scraper/job_search.py
+++ linkedin_scraper/job_search.py
@@ -10,7 +10,7 @@ from .jobs import Job
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
